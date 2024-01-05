fc58736eb745e363f7370167297c6c9f80d1f36c
EspadaAizen <jmayu85@gmail.com>
2024-01-05 11:37:47 +0530

    #update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 //import FirstPG from './components/FirstPG';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
